a1b551e187e3c7a0de3c1f04f57ec35054d972ef
oweenyin <semeyecolon@gmail.com>
2022-07-15 17:17:12 -0500

    Added better gravity

--- scene/entity/Player.gd
+++ scene/entity/Player.gd
@@ -2,15 +2,16 @@ extends KinematicBody2D
 
 export var GRAVITY = 30
 
-var velocity = Vector2()
+var velocity = Vector2.ZERO
 
 func _ready():
 	pass # Replace with function body.
 
 func _physics_process(delta):
-	velocity.y += GRAVITY
+	if !is_on_floor():
+		velocity.y += GRAVITY
 	
-	if Input.is_action_just_pressed("ui_up"):
+	if Input.is_action_just_pressed("ui_up") and is_on_floor():
 		velocity.y = -900
 	
-	velocity = move_and_slide(velocity)
+	velocity = move_and_slide(velocity, Vector2.UP)
