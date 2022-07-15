a47cc8f333c9d2c864f80dc3539485842c029f29
oweenyin <semeyecolon@gmail.com>
2022-07-15 16:43:49 -0500

    Added gravity

--- scene/entity/Player.gd
+++ scene/entity/Player.gd
@@ -1,16 +1,16 @@
 extends KinematicBody2D
 
+export var GRAVITY = 30
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+var velocity = Vector2()
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _physics_process(delta):
+	velocity.y += GRAVITY
+	
+	if Input.is_action_just_pressed("ui_up"):
+		velocity.y = -900
+	
+	velocity = move_and_slide(velocity)
