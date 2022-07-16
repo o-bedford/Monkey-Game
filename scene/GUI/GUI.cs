9034be1a45436fcee090a9e2edc640bda9dcfb0a
PersonalityMain <sonicalefan37@gmail.com>
2022-07-15 22:05:54 -0500

    Testing this and adding GUI things I believe

--- scene/GUI/GUI.cs
+++ scene/GUI/GUI.cs
@@ -6,16 +6,7 @@ public class GUI : HBoxContainer
     // Declare member variables here. Examples:
     // private int a = 2;
     // private string b = "text";
-    enum Modes
-    {
-        Simple,
-        Empty,
-        Partial
-    }
 
-    public var heartFull = ResourcePreloader("res")
-        
-        
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
